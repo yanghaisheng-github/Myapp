a0fbbe830b6fa43c412f7f63bc17dbff97aee180
yanghaisheng-github <haishengyang@outlook.com>
2020-01-08 17:25:52 +0800

    first commit1

---
+++ lib/login_screen.dart
@@ -0,0 +1,175 @@
+import 'package:flutter/scheduler.dart' show timeDilation;
+import 'package:flutter/material.dart';
+import 'package:flutter_login/flutter_login.dart';
+import 'constants.dart';
+import 'custom_route.dart';
+import 'dashboard_screen.dart';
+import 'users.dart';
+
+class LoginScreen extends StatelessWidget {
+  static const routeName = '/auth';
+
+  Duration get loginTime => Duration(milliseconds: timeDilation.ceil() * 2250);
+
+  Future<String> _loginUser(LoginData data) {
+    return Future.delayed(loginTime).then((_) {
+      if (!mockUsers.containsKey(data.name)) {
+        return '不存在该用户';
+      }
+      if (mockUsers[data.name] != data.password) {
+        return '密码不正确';
+      }
+      return null;
+    });
+  }
+
+  Future<String> _recoverPassword(String name) {
+    return Future.delayed(loginTime).then((_) {
+      if (!mockUsers.containsKey(name)) {
+        return '用户不存在';
+      }
+      return null;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final inputBorder = BorderRadius.vertical(
+      bottom: Radius.circular(10.0),
+      top: Radius.circular(20.0),
+    );
+
+    return FlutterLogin(
+      title: Constants.appName,
+      //logo: 'assets/images/ecorp.png',
+      //logoTag: Constants.logoTag,
+      //titleTag: Constants.titleTag,
+      messages: LoginMessages(
+        usernameHint: '用户名',
+        passwordHint: '密码',
+        confirmPasswordHint: '再次输入密码',
+        loginButton: '登录',
+        signupButton: '注册',
+        forgotPasswordButton: '忘记密码',
+        recoverPasswordButton: '找回密码',
+        goBackButton: '返回',
+        confirmPasswordError: '两次密码不一样，请重新输入',
+        recoverPasswordIntro: '真是三心二意，就不能认真对待我嘛？',
+        recoverPasswordDescription: '某某某',
+        recoverPasswordSuccess: '密码找回成功',
+      ),
+      // theme: LoginTheme(
+      //   primaryColor: Colors.teal,
+      //   accentColor: Colors.yellow,
+      //   errorColor: Colors.deepOrange,
+      //   pageColorLight: Colors.indigo.shade300,
+      //   pageColorDark: Colors.indigo.shade500,
+      //   titleStyle: TextStyle(
+      //     color: Colors.greenAccent,
+      //     fontFamily: 'Quicksand',
+      //     letterSpacing: 4,
+      //   ),
+      //   // beforeHeroFontSize: 50,
+      //   // afterHeroFontSize: 20,
+      //   bodyStyle: TextStyle(
+      //     fontStyle: FontStyle.italic,
+      //     decoration: TextDecoration.underline,
+      //   ),
+      //   textFieldStyle: TextStyle(
+      //     color: Colors.orange,
+      //     shadows: [Shadow(color: Colors.yellow, blurRadius: 2)],
+      //   ),
+      //   buttonStyle: TextStyle(
+      //     fontWeight: FontWeight.w800,
+      //     color: Colors.yellow,
+      //   ),
+      //   cardTheme: CardTheme(
+      //     color: Colors.yellow.shade100,
+      //     elevation: 5,
+      //     margin: EdgeInsets.only(top: 15),
+      //     shape: ContinuousRectangleBorder(
+      //         borderRadius: BorderRadius.circular(100.0)),
+      //   ),
+      //   inputTheme: InputDecorationTheme(
+      //     filled: true,
+      //     fillColor: Colors.purple.withOpacity(.1),
+      //     contentPadding: EdgeInsets.zero,
+      //     errorStyle: TextStyle(
+      //       backgroundColor: Colors.orange,
+      //       color: Colors.white,
+      //     ),
+      //     labelStyle: TextStyle(fontSize: 12),
+      //     enabledBorder: UnderlineInputBorder(
+      //       borderSide: BorderSide(color: Colors.blue.shade700, width: 4),
+      //       borderRadius: inputBorder,
+      //     ),
+      //     focusedBorder: UnderlineInputBorder(
+      //       borderSide: BorderSide(color: Colors.blue.shade400, width: 5),
+      //       borderRadius: inputBorder,
+      //     ),
+      //     errorBorder: UnderlineInputBorder(
+      //       borderSide: BorderSide(color: Colors.red.shade700, width: 7),
+      //       borderRadius: inputBorder,
+      //     ),
+      //     focusedErrorBorder: UnderlineInputBorder(
+      //       borderSide: BorderSide(color: Colors.red.shade400, width: 8),
+      //       borderRadius: inputBorder,
+      //     ),
+      //     disabledBorder: UnderlineInputBorder(
+      //       borderSide: BorderSide(color: Colors.grey, width: 5),
+      //       borderRadius: inputBorder,
+      //     ),
+      //   ),
+      //   buttonTheme: LoginButtonTheme(
+      //     splashColor: Colors.purple,
+      //     backgroundColor: Colors.pinkAccent,
+      //     highlightColor: Colors.lightGreen,
+      //     elevation: 9.0,
+      //     highlightElevation: 6.0,
+      //     shape: BeveledRectangleBorder(
+      //       borderRadius: BorderRadius.circular(10),
+      //     ),
+      //     // shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+      //     // shape: CircleBorder(side: BorderSide(color: Colors.green)),
+      //     // shape: ContinuousRectangleBorder(borderRadius: BorderRadius.circular(55.0)),
+      //   ),
+      // ),
+      emailValidator: (value) {
+        if (!value.contains('@') || !value.endsWith('.com')) {
+          return "邮箱必须包含'@'符号，也必须以'.com'结尾";
+        }
+        return null;
+      },
+      passwordValidator: (value) {
+        if (value.isEmpty) {
+          return '密码不能为空';
+        }
+        return null;
+      },
+      onLogin: (loginData) {
+        print('Login info');
+        print('Name: ${loginData.name}');
+        print('Password: ${loginData.password}');
+        return _loginUser(loginData);
+      },
+      onSignup: (loginData) {
+        print('Signup info');
+        print('Name: ${loginData.name}');
+        print('Password: ${loginData.password}');
+        return _loginUser(loginData);
+      },
+      onSubmitAnimationCompleted: () {
+        Navigator.of(context).pushReplacement(FadePageRoute(
+          builder: (context) => DashboardScreen(),
+        ));
+      },
+      onRecoverPassword: (name) {
+        print('Recover password info');
+        print('Name: $name');
+        return _recoverPassword(name);
+        // Show new password dialog
+      },
+      //showDebugButtons: true,
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'transition_route_observer.dart';
+import 'login_screen.dart';
+import 'dashboard_screen.dart';
+
+void main() {
+  //修改系统设置，以下为设置沉浸式状态栏
+  //if (Platform.isAndroid)  //需要import 'dart:io';
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      systemNavigationBarColor:
+          SystemUiOverlayStyle.dark.systemNavigationBarColor,
+    ),
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login Demo',
+      theme: ThemeData(
+        // brightness: Brightness.dark,
+        primarySwatch: Colors.deepPurple,
+        accentColor: Colors.orange,
+        cursorColor: Colors.orange,
+        // fontFamily: 'SourceSansPro',
+        textTheme: TextTheme(
+          display2: TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 45.0,
+            // fontWeight: FontWeight.w400,
+            color: Colors.orange,
+          ),
+          button: TextStyle(
+            // OpenSans is similar to NotoSans but the uppercases look a bit better IMO
+            fontFamily: 'OpenSans',
+          ),
+          caption: TextStyle(
+            fontFamily: 'NotoSans',
+            fontSize: 12.0,
+            fontWeight: FontWeight.normal,
+            color: Colors.deepPurple[300],
+          ),
+          display4: TextStyle(fontFamily: 'Quicksand'),
+          display3: TextStyle(fontFamily: 'Quicksand'),
+          display1: TextStyle(fontFamily: 'Quicksand'),
+          headline: TextStyle(fontFamily: 'NotoSans'),
+          title: TextStyle(fontFamily: 'NotoSans'),
+          subhead: TextStyle(fontFamily: 'NotoSans'),
+          body2: TextStyle(fontFamily: 'NotoSans'),
+          body1: TextStyle(fontFamily: 'NotoSans'),
+          subtitle: TextStyle(fontFamily: 'NotoSans'),
+          overline: TextStyle(fontFamily: 'NotoSans'),
+        ),
+      ),
+      home: LoginScreen(),
+      // 继承NavigatorObserver，操作Navigator的相关方法时，回调相关的操作
+      navigatorObservers: [TransitionRouteObserver()],
+      routes: {
+        LoginScreen.routeName: (context) => LoginScreen(),
+        DashboardScreen.routeName: (context) => DashboardScreen(),
+      },
+    );
+  }
+}
